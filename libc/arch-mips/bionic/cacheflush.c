6a26fbd25c7c37638fbb8b568209115a982018ab
Chris Dearman <chris@mips.com>
2010-09-29 12:06:27 -0700

    [MIPS] Android/MIPS compatible cacheflush
    Change-Id: I2bcc50613e06240fd50d54fc9c9ceb5d8c00a05b
    Signed-off-by: Chris Dearman <chris@mips.com>

--- libc/arch-mips/bionic/cacheflush.c
+++ libc/arch-mips/bionic/cacheflush.c
@@ -1,8 +1,48 @@
+#include <unistd.h>
 
-#include <sys/cachectl.h>
+#ifdef DEBUG
+#include <logd.h>
+#define  XLOG(...)   \
+	__libc_android_log_print(ANDROID_LOG_DEBUG,"libc-cacheflush",__VA_ARGS__)
+#endif
 
-int cacheflush (void *addr, const int nbytes, const int op)
+/*
+ * Linux historically defines a cacheflush(3) routine for MIPS
+ * with this signature:
+ * int cacheflush(char *addr, int nbytes, int cache);
+ *
+ * Android defines an alternate cacheflush routine which exposes the
+ * ARM system call interface:
+ * int cacheflush (long start, long end, long flags)
+ *
+ * This is an attempt to maintain compatibility between the historical MIPS
+ * usage for software previously ported to MIPS and Android specific
+ * uses of cacheflush()
+ *
+ */
+
+/* This is the Android signature */
+int cacheflush (long start, long end, long flags)
 {
-	return _flush_cache (addr, nbytes, op);
+	/*
+	 * Use the gcc __clear_cache builtin. This will generate synci
+	 * if supported or call __flush_cache directly
+	 */
+	if (end < start) {
+		/*
+		 * It looks like this is really MIPS style cacheflush call
+		 * start => addr
+		 * end => nbytes
+		 */
+#ifdef DEBUG
+		static int warned = 0;
+		if (!warned) {
+			XLOG("called with (start,len) instead of (start,end)");
+			warned = 1;
+		}
+#endif
+		end += start;
+	}
+	__clear_cache((void *)start, (void *)end);
+	return 0;
 }
-
